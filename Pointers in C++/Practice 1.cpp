1aaf9534f62931438612aee02f3905d7ecd3f94b
Faraz Siddiqui <149688343+MuhammadFarazSiddiqui@users.noreply.github.com>
2023-12-03 13:19:30 +0500

    Practice 1.cpp

--- Pointers in C++/Practice 1.cpp
+++ Pointers in C++/Practice 1.cpp
@@ -5,7 +5,7 @@ int main() {
 	int*b=&a;
 	cout<<"the address of a is"<<&a<<endl;
 	cout<<"the address of a is "<<b<<endl;
-	cout<<"the value of at address b is"<<*b<<endl;
+	cout<<"the value of a at address b is"<<*b<<endl;
 	
 	//cout<<b<<endl;
 	return 0;
